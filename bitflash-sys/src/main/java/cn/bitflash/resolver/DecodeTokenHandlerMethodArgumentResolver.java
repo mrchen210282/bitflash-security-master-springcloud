ded89ac8b8256255f65eff8eafc568b600c58a2b
chen <chenchengyi@bitflash.vip>
2018-08-08 17:53:26 +0800

    tj ccy

--- bitflash-sys/src/main/java/cn/bitflash/resolver/DecodeTokenHandlerMethodArgumentResolver.java
+++ bitflash-sys/src/main/java/cn/bitflash/resolver/DecodeTokenHandlerMethodArgumentResolver.java
@@ -1,9 +1,15 @@
 package cn.bitflash.resolver;
 
 import cn.bitflash.annotation.DecodeToken;
+import cn.bitflash.exception.RRException;
+import cn.bitflash.feignController.LoginFeign;
 import cn.bitflash.interceptor.ApiLoginInterceptor;
+import cn.bitflash.redisConfig.RedisKey;
 import cn.bitflash.user.TokenEntity;
 import cn.bitflash.utils.AESTokenUtil;
+import cn.bitflash.utils.RedisUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -14,6 +20,15 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 @Component
 public class DecodeTokenHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
 
+    @Autowired
+    private LoginFeign loginFeign;
+
+    @Autowired
+    private RedisUtils redisUtils;
+
+    private final String MOBILE = "mobile";
+    private final String UID = "uid";
+
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         return parameter.getParameterType().isAssignableFrom(TokenEntity.class)
@@ -23,16 +38,21 @@ public class DecodeTokenHandlerMethodArgumentResolver implements HandlerMethodAr
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer container, NativeWebRequest request,
                                   WebDataBinderFactory factory) throws Exception {
+        String mobile = request.getHeader(MOBILE);
+        if (mobile.length() == 0 || mobile == null) {
+            mobile = request.getParameter(MOBILE);
+        }
+        String secretUid = request.getParameter(UID);
+        if (secretUid.length() == 0 || secretUid == null) {
+            secretUid = request.getParameter(UID);
+        }
+        if (mobile == null || secretUid == null) {
+            throw new RRException("参数不能为空");
+        }
+        String token = redisUtils.get(RedisKey.LOGIN_ + mobile);
 
-        String uid = request.getParameter(ApiLoginInterceptor.UID);
-        String secretTime = request.getHeader(ApiLoginInterceptor.TIME);
-        String secretToken = request.getHeader(ApiLoginInterceptor.TOKEN);
-        String realToken=AESTokenUtil.getToken(secretTime,secretToken);
-        String realUid=AESTokenUtil.getData(realToken,uid);
-        TokenEntity token=new TokenEntity();
-        token.setToken(realToken);
-        token.setUid(realUid);
-        return token;
+        TokenEntity tokenEntity = loginFeign.getTokenByToken(token);
+
     }
 
 

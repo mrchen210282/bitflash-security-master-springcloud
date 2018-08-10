2a6337b308d7f92b096c365e503e6af2057326b0
wj <wj@bitflash.vip>
2018-08-10 16:37:40 +0800

    提交代码

--- bitflash-trade/src/main/java/cn/bitflash/controller/ApiTradeHistoryController.java
+++ bitflash-trade/src/main/java/cn/bitflash/controller/ApiTradeHistoryController.java
@@ -1,7 +1,6 @@
 package cn.bitflash.controller;
 
 import cn.bitflash.service.UserTradeHistoryService;
-import cn.bitflash.user.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ bitflash-trade/src/main/java/cn/bitflash/service/impl/UserBuyServiceImpl.java
@@ -0,0 +1,35 @@
+package cn.bitflash.service.impl;
+
+import cn.bitflash.dao.UserBuyDao;
+import cn.bitflash.service.UserBuyService;
+import cn.bitflash.trade.UserBuyEntity;
+import cn.bitflash.trade.UserBuyMessageBean;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service("userBuyService" )
+public class UserBuyServiceImpl extends ServiceImpl<UserBuyDao, UserBuyEntity> implements UserBuyService {
+
+
+    @Override
+    public List<UserBuyMessageBean> getBuyMessage(String uid, Integer pages) {
+        return baseMapper.getBuyMessage(uid, pages);
+    }
+
+    @Override
+    public Integer getNumToPaging() {
+        return baseMapper.getNumToPaging();
+    }
+
+    @Override
+    public void addBuyMessage(UserBuyEntity userBuyEntity, String uid) {
+        userBuyEntity.setUid(uid);
+        userBuyEntity.setCreate_time(new Date());
+        userBuyEntity.setState("1" );
+        baseMapper.insert(userBuyEntity);
+
+    }
+}

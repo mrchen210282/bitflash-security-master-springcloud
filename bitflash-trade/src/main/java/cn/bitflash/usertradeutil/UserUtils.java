7f77294c26a105361d9b7f54362918f551005560
chen <chenchengyi@bitflash.vip>
2018-08-29 18:17:11 +0800

    sds

---
+++ bitflash-trade/src/main/java/cn/bitflash/usertradeutil/UserUtils.java
@@ -0,0 +1,35 @@
+package cn.bitflash.usertradeutil;
+
+import cn.bitflash.user.*;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+
+@FeignClient(name = "bitflash-user",fallbackFactory = UserFallback.class)
+public interface UserUtils {
+
+    @PostMapping("/api/user/inner/selectUserInfoById")
+    public UserInfoEntity selectUserInfoById(@RequestParam("uid") String uid);
+
+    @PostMapping("/api/user/inner/selectUserInfoList")
+    public List<UserInfoEntity> selectUserInfoList(@RequestBody Map<String, Object> params);
+
+    @PostMapping("/api/user/inner/selectUserRelation")
+    public UserRelationEntity selectRelation(@RequestParam("params") Map<String, Object> params);
+
+    @PostMapping("/api/user/inner/selectUserPayPwd")
+    public UserPayPwdEntity selectUserPayPwd(@RequestBody Map<String, Object> params);
+
+    @PostMapping("/api/user/inner/selectTreeNodes")
+    public List<UserRelationJoinAccountEntity> selectTreeNodes(@RequestParam("uid") String uid);
+
+    @PostMapping("/api/user/inner/selectUserInvitationCode")
+    public UserInvitationCodeEntity selectUserInvitationCode(@RequestParam("uid") String uid);
+
+    @PostMapping("/api/user/inner//selectUserPayUrl")
+    public List<UserPayUrlEntity> selectUserPayUrl(@RequestParam("uid")String uid);
+}
